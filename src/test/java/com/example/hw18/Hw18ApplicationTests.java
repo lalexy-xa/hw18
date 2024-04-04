17e6d0a1dfcf299d2953a8a657740f93a8122819
ttLala <lalexy.dn@gmail.com>
2024-04-04 22:38:23 +0300

    test1

--- src/test/java/com/example/hw18/Hw18ApplicationTests.java
+++ src/test/java/com/example/hw18/Hw18ApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.hw18;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class Hw18ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, true);
 	}
 
 }
